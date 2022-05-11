300f71d8d7ad3fd3e9bfe1ab5a00b69889e1b7b4
seungduru <dlatmddn468@gmail.com>
2022-05-11 13:12:15 +0900

    create Address

---
+++ src/main/java/com/example/bookmanager/domain/Address.java
@@ -0,0 +1,26 @@
+package com.example.bookmanager.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+@Embeddable
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+
+    private String city;        // 시
+
+    private String district;    // 구
+
+    @Column(name = "address_detail")
+    private String detail;      // 상세주소
+
+    private String zipCode;     // 우편번호
+}
