289186f3bf862321be2e6b2b0ce491cbe999dc52
seungduru <dlatmddn468@gmail.com>
2022-05-29 23:44:55 +0900

    create CommentRepository

---
+++ src/test/java/com/example/bookmanager/repository/CommentRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.example.bookmanager.repository;
+
+import com.example.bookmanager.domain.Comment;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CommentRepositoryTest {
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Test
+    @Transactional
+    void commentTest() {
+        Comment comment = new Comment();
+        comment.setComment("별로예요");
+//        comment.setCommentedAt(LocalDateTime.now());
+
+        commentRepository.saveAndFlush(comment);
+
+        entityManager.clear();
+
+//        System.out.println(commentRepository.findById(3L).get());
+
+        System.out.println(comment);
+
+        commentRepository.findAll().forEach(System.out::println);
+    }
+}
