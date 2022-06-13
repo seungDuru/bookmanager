0efac3540dc37e13cfb1e9aa431062cdfaf9a4d1
seungduru <dlatmddn468@gmail.com>
2022-06-14 08:40:48 +0900

    create BookStatus

---
+++ src/main/java/com/example/bookmanager/repository/dto/BookStatus.java
@@ -0,0 +1,32 @@
+package com.example.bookmanager.repository.dto;
+
+import lombok.Data;
+
+@Data
+public class BookStatus {
+
+    private int code;
+    private String description;
+
+    public BookStatus(int code){
+        this.code = code;
+        this.description = parseDescription(code);
+    }
+
+    public boolean isDisplayed(){
+        return code == 200;
+    }
+
+    private String parseDescription(int code){
+        switch (code){
+            case 100 :
+                return "판매종룜";
+            case 200 :
+                return "판매중";
+            case 300 :
+                return "판매보류";
+            default:
+                return "미지원";
+        }
+    }
+}
