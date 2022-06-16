f177fcf7342669ea55deded43fdd2fc1a328c666
seungduru <dlatmddn468@gmail.com>
2022-06-16 19:04:06 +0900

    create BookStatusConverter

---
+++ src/main/java/com/example/bookmanager/domain/converter/BookStatusConverter.java
@@ -0,0 +1,19 @@
+package com.example.bookmanager.domain.converter;
+
+import com.example.bookmanager.repository.dto.BookStatus;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter(autoApply = true)
+public class BookStatusConverter implements AttributeConverter<BookStatus, Integer> {
+    @Override
+    public Integer convertToDatabaseColumn(BookStatus attribute) {
+        return attribute.getCode();
+    }
+
+    @Override
+    public BookStatus convertToEntityAttribute(Integer dbData) {
+        return dbData != null ? new BookStatus(dbData) : null;
+    }
+}
