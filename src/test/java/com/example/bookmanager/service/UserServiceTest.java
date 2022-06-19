31b635aaf647c5b40f1e0dc6e6d8042db3f6cfb5
seungduru <dlatmddn468@gmail.com>
2022-06-19 22:49:37 +0900

    create UserServiceTest

---
+++ src/test/java/com/example/bookmanager/service/UserServiceTest.java
@@ -0,0 +1,25 @@
+package com.example.bookmanager.service;
+
+import com.example.bookmanager.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserServiceTest {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void test(){
+        userService.put();
+
+//        System.out.println(">>> " + userRepository.findByEmail("newUser@fastcampus.com"));
+        userRepository.findAll().forEach(System.out::println);
+    }
+}
