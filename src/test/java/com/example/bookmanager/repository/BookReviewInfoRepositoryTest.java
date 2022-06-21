dd96cef353c9469d2539e0693126ce4c53531186
seungduru <dlatmddn468@gmail.com>
2022-06-21 15:01:52 +0900

    create BookReviewInfoRepositoryTest

---
+++ src/test/java/com/example/bookmanager/repository/BookReviewInfoRepositoryTest.java
@@ -0,0 +1,66 @@
+package com.example.bookmanager.repository;
+
+import com.example.bookmanager.domain.Book;
+import com.example.bookmanager.domain.BookReviewInfo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BookReviewInfoRepositoryTest {
+    @Autowired
+    private BookReviewInfoRepository bookReviewInfoRepository;
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Test
+    void crudTest() {
+        BookReviewInfo bookReviewInfo = new BookReviewInfo();
+//        bookReviewInfo.setBookId(1L);
+        bookReviewInfo.setBook(givenBook());
+        bookReviewInfo.setAverageReviewScore(4.5f);
+        bookReviewInfo.setReviewCount(2);
+
+        bookReviewInfoRepository.save(bookReviewInfo);
+
+        System.out.println(">>> " + bookReviewInfoRepository.findAll());
+    }
+
+    @Test
+    void crudTest2(){
+        givenBookReviewInfo();
+
+        Book result = bookReviewInfoRepository.findById(1L)
+                .orElseThrow(RuntimeException::new)
+                .getBook();
+
+        System.out.println(">>> " + result);
+
+        BookReviewInfo result2 = bookRepository.findById(7L)
+                .orElseThrow(RuntimeException::new)
+                .getBookReviewInfo();
+
+        System.out.println(">>> " + result2);
+    }
+
+    private Book givenBook(){
+        Book book = new Book();
+        book.setName("Jpa 초격차 패키지");
+        book.setAuthorId(1L);
+
+        return bookRepository.save(book);
+    }
+
+    private void givenBookReviewInfo(){
+        BookReviewInfo bookReviewInfo = new BookReviewInfo();
+        bookReviewInfo.setBook(givenBook());
+        bookReviewInfo.setAverageReviewScore(4.5f);
+        bookReviewInfo.setReviewCount(2);
+
+        bookReviewInfoRepository.save(bookReviewInfo);
+
+        System.out.println(">>> " + bookReviewInfoRepository.findAll());
+    }
+}
