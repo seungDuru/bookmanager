48888957b8739f1669ce5848db66228468278eec
seungduru <dlatmddn468@gmail.com>
2022-06-28 20:53:35 +0900

    create EntityManagerTest

---
+++ src/test/java/com/example/bookmanager/service/EntityManagerTest.java
@@ -0,0 +1,50 @@
+package com.example.bookmanager.service;
+
+import com.example.bookmanager.domain.User;
+import com.example.bookmanager.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+@SpringBootTest
+//@Transactional
+public class EntityManagerTest {
+    @Autowired
+    private EntityManager entityManager;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void entityManagerTest(){
+        System.out.println(entityManager.createQuery("select u from User u").getResultList());
+    }
+
+    @Test
+    void cacheFindTest(){
+//        System.out.println(userRepository.findByEmail("martin@fastcampus.com"));
+//        System.out.println(userRepository.findByEmail("martin@fastcampus.com"));
+//        System.out.println(userRepository.findByEmail("martin@fastcampus.com"));
+//        System.out.println(userRepository.findById(2L).get());
+//        System.out.println(userRepository.findById(2L).get());
+//        System.out.println(userRepository.findById(2L).get());
+
+        userRepository.deleteById(1L);
+    }
+
+    @Test
+    void cacheFindTest2(){
+        User user = userRepository.findById(1L).get();
+        user.setName("marrrrrrrtin");
+        userRepository.save(user);
+
+        System.out.println("---------------------");
+
+        user.setEmail("marrrrrrtin@fastcampus.com");
+        userRepository.save(user);
+
+        System.out.println(userRepository.findAll());
+    }
+}
